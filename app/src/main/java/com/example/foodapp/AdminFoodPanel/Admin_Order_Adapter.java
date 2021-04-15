9061f3068b097706098c7631ee16400850e0d7d4
syedbabermehmood <syedbabermehmood26@gmail.com>
2021-04-15 22:02:35 +0500

    Order Update Status/ Notification

--- app/src/main/java/com/example/foodapp/AdminFoodPanel/Admin_Order_Adapter.java
+++ app/src/main/java/com/example/foodapp/AdminFoodPanel/Admin_Order_Adapter.java
@@ -1,20 +1,32 @@
 package com.example.foodapp.AdminFoodPanel;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.foodapp.Model.Request_Order_Model;
 import com.example.foodapp.R;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.jaredrummler.materialspinner.MaterialSpinner;
+
 
 public class Admin_Order_Adapter extends FirebaseRecyclerAdapter<Request_Order_Model, Admin_Order_Adapter.myViewHolder> {
 
+    MaterialSpinner spinner;
+    DatabaseReference dbReference= FirebaseDatabase.getInstance().getReference().child("Order_Requests");
 
     public Admin_Order_Adapter(@NonNull FirebaseRecyclerOptions<Request_Order_Model> options) {
         super(options);
@@ -25,7 +37,28 @@ public class Admin_Order_Adapter extends FirebaseRecyclerAdapter<Request_Order_M
         myViewHolder.order_ID.setText("ID# "+getRef(position).getKey());
         myViewHolder.order_Status.setText(convertCodeToStatus(request_order_model.getStatus()));
         myViewHolder.order_Address.setText(request_order_model.getAddress());
+        myViewHolder.customerName.setText(request_order_model.getName());
+
+
+        myViewHolder.btn_editStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showUpdateDialog(v.getContext(), getRef(position).getKey(),getItem(position));
+            }
+        });
+
+        myViewHolder.btn_deleteOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbReference.child(getRef(position).getKey()).removeValue();
+            }
+        });
+
+
     }
+
+
 
     @NonNull
     @Override
@@ -36,13 +69,17 @@ public class Admin_Order_Adapter extends FirebaseRecyclerAdapter<Request_Order_M
     }
 
     public class myViewHolder extends RecyclerView.ViewHolder {
-        TextView order_ID, order_Status, order_Address;
+        TextView order_ID, order_Status, order_Address, customerName;
+        Button btn_editStatus, btn_deleteOrder;
 
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
             order_ID= itemView.findViewById(R.id.order_ID);
             order_Status= itemView.findViewById(R.id.order_Status);
             order_Address= itemView.findViewById(R.id.order_Address);
+            customerName= itemView.findViewById(R.id.customerName);
+            btn_editStatus= itemView.findViewById(R.id.btn_editStatus);
+            btn_deleteOrder= itemView.findViewById(R.id.btn_deleteOrder);
         }
     }
 
@@ -59,4 +96,44 @@ public class Admin_Order_Adapter extends FirebaseRecyclerAdapter<Request_Order_M
 
         return txt_Status;
     }
+
+
+    // Edit Order Status DialogBox
+    private void showUpdateDialog(Context context, String key, Request_Order_Model item) {
+
+        final AlertDialog.Builder alertDialog= new AlertDialog.Builder(context);
+        alertDialog.setTitle("Update Order Status");
+        alertDialog.setMessage("Please choose the status.");
+
+        LayoutInflater inflater= LayoutInflater.from(context);
+        final View view= inflater.inflate(R.layout.admin_update_order_spinner_layout,null);
+
+        spinner= view.findViewById(R.id.status_Spinner);
+        spinner.setItems("Order Placed", "On My Way", "Delivered");
+
+        alertDialog.setView(view);
+
+        final String localKey= key;
+
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                item.setStatus(String.valueOf(spinner.getSelectedIndex()));
+
+                dbReference.child(localKey).setValue(item);
+            }
+        });
+
+        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        alertDialog.show();
+    }
+
+
 }
---
+++ app/src/main/java/com/example/foodapp/AdminFoodPanel/Service/ListenOrder.java
@@ -0,0 +1,16 @@
+package com.example.foodapp.AdminFoodPanel.Service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ListenOrder extends Service {
+    public ListenOrder() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
